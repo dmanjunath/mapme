4c5c2467d5382897e85400871e347cd7f0ae3d17
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2014-08-14 10:55:04 -0400

    semicolons

--- mapme.js
+++ mapme.js
@@ -7,27 +7,27 @@
   
   by: Hareesh Nagaraj & Dheeraj Manjunath
 */
-var parseArgs = require('minimist')          //Using the minimist node package 
-var fs = require('fs')                       //Requiring the basic file system
-var fileParser = require('./fileParser.js')  //fileParser is the class used to parse the page for dependencies
-var argArray = parseArgs(process.argv.slice(2))  //Grabbing the command line arguments
-var argv = argArray['_']
+var parseArgs = require('minimist');         //Using the minimist node package 
+var fs = require('fs');                      //Requiring the basic file system
+var fileParser = require('./fileParser.js');  //fileParser is the class used to parse the page for dependencies
+var argArray = parseArgs(process.argv.slice(2));  //Grabbing the command line arguments
+var argv = argArray['_'];
 
-var map = {}
-var parser = new fileParser(argv)
-parser.fileContainer = map
+var map = {};
+var parser = new fileParser(argv);
+parser.fileContainer = map;
 
-var a = 0
+var a = 0;
 var result = parser.parse(function(result){
-   console.log("Generating..."+a)
+   console.log("Generating..."+a);
    // console.log(result)
-   a++
+   a++;
    createPage(result);
-})
+});
 
 function createPage(result){
-  var pageCreator = require('./pageCreator.js')  //pageCreator is the class that generates the D3 page
-  var page = new pageCreator(result)
-  pageCreator.parent = result.root
-  page.create()
+  var pageCreator = require('./pageCreator.js');  //pageCreator is the class that generates the D3 page
+  var page = new pageCreator(result);
+  pageCreator.parent = result.root;
+  page.create();
 }
