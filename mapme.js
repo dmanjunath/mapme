83f526fbefd216753d2a3748cc1b1706f51a89cc
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2014-08-17 16:25:11 -0400

    traversal and file opening errors handled

--- mapme.js
+++ mapme.js
@@ -1,5 +1,7 @@
 var parseArgs = require('minimist');              //Using the minimist node package 
 var fileParser = require('./lib/fileParser.js');
+var TreeNode = require("tree-node");
+
 var argArray = parseArgs(process.argv.slice(2));  //Grabbing the command line arguments
 var argv = argArray._;
 var writeFile = 'dependencies.html';              //the default file being written to
@@ -12,6 +14,9 @@ else {
   if(argv[1]){
     writeFile = argv[1];
   }
-  var fp = new fileParser(readFile);
-  fp.parse();
+  var rootNode = new TreeNode(readFile);
+  var fp = new fileParser(readFile,rootNode,rootNode);
+  fp.parse(function(err,data){
+    console.log(data);
+  });
 }
