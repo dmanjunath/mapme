bfa97891cc66848b5c4e015f7ed56d8731518cba
Dheeraj Manjunath <dheerajmanju1@yahoo.com>
2014-08-18 01:31:46 -0400

    logic for page creator

--- mapme.js
+++ mapme.js
@@ -5,6 +5,7 @@ var TreeNode = require("tree-node");
 var argArray = parseArgs(process.argv.slice(2));  //Grabbing the command line arguments
 var argv = argArray._;
 var writeFile = 'dependencies.html';              //the default file being written to
+var dataObj = {};
 
 if(argv.length < 1 || argv.length > 2){
   console.log("Incorrect arguments");
@@ -17,6 +18,33 @@ else {
   var rootNode = new TreeNode(readFile);
   var fp = new fileParser(readFile,rootNode,rootNode,__dirname);
   fp.parse(function(err,data){
-    console.log(data);
+    // call pagecreator and pass in the rootnode here
+    // data is the root node
+    createJSON(data, data, dataObj, function(){
+      console.log(JSON.stringify(dataObj));
+    });
   });
+}
+
+// this should go in the pageCreator file
+function createJSON(rootNode, currentNode, currentState, callback){
+  var state;
+  if(currentNode === rootNode){
+    dataObj.name = rootNode._id;
+    dataObj.children = [];
+    state = dataObj.children;
+    currentState = dataObj.children;
+  }
+
+  currentNode._childIdsList.forEach(function(id){
+    var node = rootNode.getNode(id);
+    if(node._childs.length !== 0){
+      currentState.push({name: node._id, children: []});
+      state = currentState[currentState.length - 1].children;
+    }
+    createJSON(rootNode, node, state, function(){
+
+    });
+  });
+  callback(null);
 }
